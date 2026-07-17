a2b100550239ee8a6358d1063edcadda7e01a35d
admin <fernandoparadoja@gmail.com>
2026-07-16 23:58:12 -0500

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from "./pages/Home";
-import EventList from "./pages/EventList";
 import Checkout from "./pages/Checkout";
 import UploadPayment from "./pages/UploadPayment";
 import TicketStatus from "./pages/TicketStatus";
-import EventDetail from "./pages/EventDetail";
-import Contact from "./pages/Contact";
-import Partners from "./pages/Partners";
 
 import StaffLogin from "./pages/StaffLogin";
 import StaffScanner from "./pages/StaffScanner";
@@ -15,29 +10,30 @@ import StaffValidator from "./pages/StaffValidator";
 import StaffAdmin from "./pages/StaffAdmin";
 import StaffUnauthorized from "./pages/StaffUnauthorized";
 
-import MainLayout from "./layout/MainLayout";
 import StaffLayout from "./layout/StaffLayout";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+
+const EVENT_ID = "072bc678-2eb1-4a0d-8e0f-e8f1dff219d0";
 
 function App() {
   return (
     <Routes>
-      {/* Páginas con header y footer */}
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<EventList />} />
-        <Route path="/event/:eventId" element={<EventDetail />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/partners" element={<Partners />} />
-      </Route>
+      {/* Redirecciones */}
+      <Route path="/" element={<Navigate to={`/checkout/${EVENT_ID}`} replace />} />
+      <Route path="/events" element={<Navigate to={`/checkout/${EVENT_ID}`} replace />} />
+      <Route path="/event/:eventId" element={<Navigate to={`/checkout/${EVENT_ID}`} replace />} />
+      <Route path="/contact" element={<Navigate to={`/checkout/${EVENT_ID}`} replace />} />
+      <Route path="/partners" element={<Navigate to={`/checkout/${EVENT_ID}`} replace />} />
 
-      {/* Páginas sin header ni footer */}
+      {/* Flujo de compra */}
       <Route path="/checkout/:eventId" element={<Checkout />} />
       <Route path="/payment/:orderId" element={<UploadPayment />} />
       <Route path="/ticket/:code" element={<TicketStatus />} />
+
+      {/* Login */}
       <Route path="/staff/login" element={<StaffLogin />} />
 
-      {/* Rutas protegidas del staff */}
+      {/* Staff */}
       <Route element={<ProtectedRoute />}>
         <Route element={<StaffLayout />}>
           <Route element={<ProtectedRoute roles={["admin"]} />}>
